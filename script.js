aa1f7ed0925f2f67dd5ba13d9046bec40dc7f3f2
bachinskim <250670+bachinskim@users.noreply.github.com>
2025-03-27 00:54:29 +0100

    Added assignment skeleton

---
+++ script.js
@@ -0,0 +1,103 @@
+/*
+Mapping from MealDB Categories to TheCocktailDB drink ingredient
+You can customize or expand this object to suit your needs.
+*/
+const mealCategoryToCocktailIngredient = {
+  Beef: "whiskey",
+  Chicken: "gin",
+  Dessert: "amaretto",
+  Lamb: "vodka",
+  Miscellaneous: "vodka",
+  Pasta: "tequila",
+  Pork: "tequila",
+  Seafood: "rum",
+  Side: "brandy",
+  Starter: "rum",
+  Vegetarian: "gin",
+  Breakfast: "vodka",
+  Goat: "whiskey",
+  Vegan: "rum",
+  // Add more if needed; otherwise default to something like 'cola'
+};
+
+/*
+    2) Main Initialization Function
+       Called on page load to start all the requests:
+       - Fetch random meal
+       - Display meal
+       - Map meal category to spirit
+       - Fetch matching (or random) cocktail
+       - Display cocktail
+*/
+function init() {
+  fetchRandomMeal()
+    .then((meal) => {
+      displayMealData(meal);
+      const spirit = mapMealCategoryToDrinkIngredient(meal.strCategory);
+      return fetchCocktailByDrinkIngredient(spirit);
+    })
+    .then((cocktail) => {
+      displayCocktailData(cocktail);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+/*
+ Fetch a Random Meal from TheMealDB
+ Returns a Promise that resolves with the meal object
+ */
+function fetchRandomMeal() {
+    // Fill in
+}
+
+/*
+Display Meal Data in the DOM
+Receives a meal object with fields like:
+  strMeal, strMealThumb, strCategory, strInstructions,
+  strIngredientX, strMeasureX, etc.
+*/
+function displayMealData(meal) {
+    // Fill in
+}
+
+/*
+Convert MealDB Category to a TheCocktailDB Spirit
+Looks up category in our map, or defaults to 'cola'
+*/
+function mapMealCategoryToDrinkIngredient(category) {
+  if (!category) return "cola";
+  return mealCategoryToCocktailIngredient[category] || "cola";
+}
+
+/*
+Fetch a Cocktail Using a Spirit from TheCocktailDB
+Returns Promise that resolves to cocktail object
+We call https://www.thecocktaildb.com/api/json/v1/1/search.php?s=DRINK_INGREDIENT to get a list of cocktails
+Don't forget encodeURIComponent()
+If no cocktails found, fetch random
+*/
+function fetchCocktailByDrinkIngredient(drinkIngredient) {
+    // Fill in
+}
+
+/*
+Fetch a Random Cocktail (backup in case nothing is found by the search)
+Returns a Promise that resolves to cocktail object
+*/
+function fetchRandomCocktail() {
+    // Fill in
+}
+
+/*
+Display Cocktail Data in the DOM
+*/
+function displayCocktailData(cocktail) {
+    // Fill in
+}
+
+/*
+Call init() when the page loads
+*/
+window.onload = init;
